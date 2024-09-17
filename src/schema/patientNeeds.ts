9733bc9cc9faa356433f35b3e88b01520e9e3c13
nXhermane <nxhermane@gmail.com>
2024-09-17 14:03:10 +0100

    add nutritionFormular to db

--- src/schema/patientNeeds.ts
+++ src/schema/patientNeeds.ts
@@ -1,12 +1,24 @@
 import { sql } from "drizzle-orm";
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+export type NutritionalRef = {
+  condition: string;
+  weight: number;
+  bme?: number;
+  anr?: number;
+  amt?: number;
+  as?: number;
+};
+export type Variables = {
+  [variableAlias: string]: string;
+};
 export const nutritionRules = sqliteTable("nutrition_rules", {
   id: text("id").primaryKey(),
   name: text("name", { length: 255 }),
   nameF: text("nameF", { length: 255 }),
   code: text("code", { length: 100 }).notNull().unique(),
   formula: text("formula").notNull(),
+
   createdAt: text("createdAt")
     .notNull()
     .default(sql`CURRENT_TIMESTAMP`),
@@ -16,9 +28,35 @@ export const nutritionRules = sqliteTable("nutrition_rules", {
     .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
 });
 
-export const nutritiontionalReferencesValues = sqliteTable("nutritional_reference_values",{
+export const nutritiontionalReferencesValues = sqliteTable(
+  "nutritional_reference_values",
+  {
+    id: text("id").primaryKey(),
+    tagname: text("nutrientTagname"),
+    origin: text("origin"),
+    value: text("value", { mode: "json" }).$type<NutritionalRef[]>(),
+    conditionVariables: text('variables',{mode:'json'}).$type<Variables>(),
+    unit: text("unit").notNull(),
+    createdAt: text("createdAt")
+      .notNull()
+      .default(sql`CURRENT_TIMESTAMP`),
+    updatedAt: text("updatedAt")
+      .notNull()
+      .default(sql`CURRENT_TIMESTAMP`)
+      .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
+  }
+);
+export const nutritionFormulars = sqliteTable("nutrition_formulars", {
   id: text("id").primaryKey(),
-  tagname: text("nutrientTagname"),
-
-  
-})
+  name: text("formularName").notNull(),
+  expression: text("formularExpression").notNull(),
+  condition: text("formularCondition").default("1"),
+  variables: text("variables", { mode: "json" }).$type<Variables>(),
+  createdAt: text("createdAt")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: text("updatedAt")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`)
+    .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
+});
